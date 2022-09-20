b7dca297e771c40f0183ed35024f4c1fdf775829
ilyas <mohamed_ilyas_fakrudeen_abdul_salam@sp.edu.sg>
2022-09-20 14:10:57 +0800

    lab2 code completed.

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,29 @@
-print("ET0735 (DEVOps for AIoT) - Lab 2 Introduction to Python!!")
+def main():
+    print("ET0735 (DEVOps for AIoT) - Lab 2 Introduction to P")
+    display_main_menu()
+    num_list = get_user_input()
+    print(num_list)
+    print(calc_average_temperature(num_list))
+    print(calc_min_max_temperature(num_list))
+
+def display_main_menu():
+    print("enter some numbers separated by commas")
+    return
+
+def get_user_input():
+    numInp = input()
+    inpList = numInp.split(",")
+    return inpList
+
+def calc_average_temperature( tempList ):
+    est_list = list(map(int, tempList))
+    return sum(est_list)/len(est_list)
+
+def calc_min_max_temperature(tempList):
+    est_list = list(map(int, tempList))
+    min_max_list = [max(est_list), min(est_list)]
+    return min_max_list
+
+
+if __name__ == "__main__":
+    main()
