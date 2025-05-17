02404a7eff44e7987a0ba85a02cf2fbcad263d1e
ilyas <mohamed_ilyas_fakrudeen_abdul_salam@sp.edu.sg>
2025-05-17 23:21:27 +0800

    complete ex 5.

--- Lab2.py
+++ Lab2.py
@@ -1,28 +1,35 @@
+
+
 def main():
-    print("ET0735 (DEVOps for AIoT) - Lab 2 Introduction to P")
+    print("ET0735 (DEVOps for AIoT) - Lab 2 Introduction to Python Programming")
     display_main_menu()
     num_list = get_user_input()
     print(num_list)
-    print(calc_average_temperature(num_list))
-    print(calc_min_max_temperature(num_list))
+    print("Average Temperature:", calc_average_temperature(num_list))
+    print("Min and Max Temperature:", calc_min_max_temperature(num_list))
+    print("Sorted Temperature List:", sort_temperature_list(num_list))
+
 
 def display_main_menu():
-    print("enter some numbers separated by commas 1")
+    print("enter some numbers separated by commas : ")
     return
 
 def get_user_input():
     numInp = input()
     inpList = numInp.split(",")
-    return inpList
+    est_list = list(map(float, inpList))
+    return est_list
 
-def calc_average_temperature( tempList ):
-    est_list = list(map(int, tempList))
-    return sum(est_list)/len(est_list)
+def calc_average_temperature( tempList):
+    return sum(tempList)/len(tempList)
 
 def calc_min_max_temperature(tempList):
-    est_list = list(map(int, tempList))
-    min_max_list = [max(est_list), min(est_list)]
+    min_max_list = [min(tempList), max(tempList)]
     return min_max_list
+
+def sort_temperature_list(tempList):
+    return sorted(tempList)
+
 
 
 if __name__ == "__main__":
